cdae9fb3aca255e72788f05420aea12b728ad5ea
igor matos ribeiro <igortecnologia2020@gmail.com>
2020-09-04 22:53:00 -0300

    a mensagem foi enviada atividade completa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _EtecAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etec Adolpho Berezin");
+        }
+    }
+}
